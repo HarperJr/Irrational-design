91933305b49ec3c167260d3a4946642383d60bea
Nikita Sychev <ns@digipeople.ru>
2019-03-09 11:54:24 -0800

    #Деталка поста
    - Предварительная верстка шаблона

---
+++ src/presentation/post/detail/SideBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <div class="artist-block">
+        <picture>
+          
+        </picture>
+        <h2>Michael Greenwood</h2>
+        <h3>michaelGreen@gpost.com</h3>
+      </div>
+      <button type="button" name="btn_follow">+Follow</button>
+    </div>
+    <div class="info">
+      <div class="btn_group_fav_like">
+
+      </div>
+      <h1>Facepack monster</h1>
+      <p>Description</p>
+      <div class="tabs">
+        <ul class="tags">
+          <li class="tag">Art</li>
+          <li class="tag">Anime</li>
+        </ul>
+      </div>
+    </div>
+  </div
+</template>
+
+<style>
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+</style>
