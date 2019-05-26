ad2d29b336989f7b4bb6345a96a4d903a01966e2
NonameNoname123 <zhenya.grigorieva116@yandex.ru>
2019-05-26 23:01:56 +0300

    #Страница создания поста - теперь создаются посты

--- src/components/createpost/Createpost.vue
+++ src/components/createpost/Createpost.vue
@@ -32,7 +32,7 @@
                             solo></v-text-field>
                     <v-autocomplete
                             v-model="postPayload.post.categories"
-                            :items="states"
+                            :items="categories"
                             label="Категории"
                             multiple
                             single-line
@@ -50,7 +50,7 @@
                             :items="items"
                             :search-input.sync="search"
                             hide-selected
-                            label="Search for an option"
+                            label="Выберите тег"
                             multiple
                             small-chips
                             solo
@@ -172,22 +172,7 @@
         model: null,
         errors: [],
         tags: [],
-        states: [
-          'Alabama', 'Alaska', 'American Samoa', 'Arizona',
-          'Arkansas', 'California', 'Colorado', 'Connecticut',
-          'Delaware', 'District of Columbia', 'Federated States of Micronesia',
-          'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho',
-          'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
-          'Louisiana', 'Maine', 'Marshall Islands', 'Maryland',
-          'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-          'Missouri', 'Montana', 'Nebraska', 'Nevada',
-          'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
-          'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
-          'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
-          'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
-          'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
-          'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-        ],
+        categories: this.$store.getters.categories,
         imageData: "",
         postPayload: {
           post: {
@@ -253,7 +238,11 @@
       })
     }
   },
-  methods: {
+    created() {
+      this.$store.dispatch('get_categories', this.categories);
+      console.log(this.$store.getters.categories);
+    },
+    methods: {
     edit (index, item) {
       if (!this.editing) {
         this.editing = item
