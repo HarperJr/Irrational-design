4736cce78c7a90175acf4d90764cb1599bc7705e
NonameNoname123 <zhenya.grigorieva116@yandex.ru>
2019-06-03 18:20:44 +0300

    #Страница поста -отправление комментария

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -4,56 +4,56 @@
       <v-flex xs12 md5 class="post-card">
         <v-card class="v-post-card">
 
-            <v-layout column class="post-card-text">
+          <v-layout column class="post-card-text">
+            <v-flex>
               <v-flex>
-                <v-flex>
-                  <v-avatar size="100px" style="margin-bottom: 7px">
-                    <img :src="avatarUrl" >
-                  </v-avatar>
-                </v-flex>
-                <v-flex align-center offset-xs1>
-                  <strong><a class="permalink" :href="'/#/profile/'+post.artist.id">{{post.artist.name}}</a></strong>
-                </v-flex>
+                <v-avatar size="100px" style="margin-bottom: 7px">
+                  <img :src="avatarUrl">
+                </v-avatar>
               </v-flex>
-              <v-flex>
-                <h2>{{post.artist.email}}</h2>
+              <v-flex align-center offset-xs1>
+                <strong><a class="permalink" :href="'/#/profile/'+post.artist.id">{{post.artist.name}}</a></strong>
               </v-flex>
+            </v-flex>
+            <v-flex>
+              <h2>{{post.artist.email}}</h2>
+            </v-flex>
 
-              <v-card-actions class="post-actions">
-                <v-btn depressed class="post-btn post-btn-follow" @click="follow">+Подписаться</v-btn>
-                <!--div>кнопка должна быть активна, если пользователь уже подписался</div-->
-                <!--v-btn depressed class="post-btn post-btn-followed" @click="follow">Followed</v-btn-->
-                <!--div>эту кнопку пока опустим<div-->
-                <!--v-btn depressed class="post-btn post-btn-likes" @click="bookmark">Bookmark</v-btn-->
-                <v-btn depressed class="post-btn post-btn-likes" @click="like">Like</v-btn>
-              </v-card-actions>
+            <v-card-actions class="post-actions">
+              <v-btn depressed class="post-btn post-btn-follow" @click="follow">+Подписаться</v-btn>
+              <!--div>кнопка должна быть активна, если пользователь уже подписался</div-->
+              <!--v-btn depressed class="post-btn post-btn-followed" @click="follow">Followed</v-btn-->
+              <!--div>эту кнопку пока опустим<div-->
+              <!--v-btn depressed class="post-btn post-btn-likes" @click="bookmark">Bookmark</v-btn-->
+              <v-btn depressed class="post-btn post-btn-likes" @click="like">Like</v-btn>
+            </v-card-actions>
 
-              <h1 class="v-card-h">{{post.title}}</h1>
-              <h2 class="v-card-h">{{post.subtitle}}</h2>
-              <p class="v-card-h v-card-tarea">{{post.description}}</p>
-              <v-textarea class="v-card-h"
-                      v-model="comment"
-                      required
-                      label="Добавить комментарий"
-                      single-line
+            <h1 class="v-card-h">{{post.title}}</h1>
+            <h2 class="v-card-h">{{post.subtitle}}</h2>
+            <p class="v-card-h v-card-tarea">{{post.description}}</p>
+            <v-textarea class="v-card-h"
+                        v-model="comment"
+                        required
+                        label="Добавить комментарий"
+                        single-line
 
-                      solo></v-textarea>
+                        solo></v-textarea>
+            <v-btn depressed class="post-btn post-btn-unblock" @click="submit">Отправить</v-btn>
+            <v-card-actions class="post-actions">
 
-              <v-card-actions class="post-actions">
+              <v-btn v-if="isAuthor" depressed class="post-btn post-btn-delete" @click="remove">Удалить</v-btn>
+              <!--v-btn v-else depressed class="post-btn post-btn-report" @click="report">Пожаловаться</v-btn-->
+              <!--v-btn v-else depressed class="post-btn post-btn-report" @click="report">Вы пожаловались</v-btn-->
+            </v-card-actions>
+            <!--v-card-actions class="post-actions">
+              <v-btn depressed class="post-btn post-btn-block" @click="block">Блокировать</v-btn>
+              <v-btn depressed class="post-btn post-btn-unblock" @click="unblock">Разблокировать</v-btn>
+            </v-card-actions-->
 
-                <v-btn v-if="isAuthor" depressed class="post-btn post-btn-delete" @click="remove">Удалить</v-btn>
-                <!--v-btn v-else depressed class="post-btn post-btn-report" @click="report">Пожаловаться</v-btn-->
-                <!--v-btn v-else depressed class="post-btn post-btn-report" @click="report">Вы пожаловались</v-btn-->
-              </v-card-actions>
-              <!--v-card-actions class="post-actions">
-                <v-btn depressed class="post-btn post-btn-block" @click="block">Блокировать</v-btn>
-                <v-btn depressed class="post-btn post-btn-unblock" @click="unblock">Разблокировать</v-btn>
-              </v-card-actions-->
-
-              <v-layout column>
-                <Comment v-for="comment in comments" :key="comment.id" :comment="comment"></Comment>
-              </v-layout>
+            <v-layout column>
+              <Comment v-for="comment in comments" :key="comment.id" :comment="comment"></Comment>
             </v-layout>
+          </v-layout>
 
         </v-card>
       </v-flex>
@@ -76,6 +76,7 @@
     name: 'Post',
     data() {
       return {
+        comment: '',
         post: null,
         comments: {
           type: Object,
@@ -95,6 +96,16 @@
     },
     components: {
       Comment, Art
+    },
+    methods: {
+      submit() {
+        http.post(`post/comment`, {
+              postId: this.post.id,
+              content: this.comment
+            }
+        ).then(res => {
+        }).catch(ex => console.log(ex))
+      }
     },
     mounted() {
       http.get(`post/${this.$route.params.id}`)
